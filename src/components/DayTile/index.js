065cf9dacca23c692efaefed8bb3a89e956f99d2
Andreas Ölvebo <andreas.olvebo@gmail.com>
2018-07-04 13:40:43 +0200

    added DayTile/index.js

---
+++ src/components/DayTile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class DayTile extends React.Component {
+    render() {
+        return (
+            <div>DayTile</div>
+        )
+    }
+}
+
+export default DayTile
