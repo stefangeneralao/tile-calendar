e95b302bd5564b776c725b0a599e7a8d598aa473
stefangeneralao <stefan.generalao@gmail.com>
2018-07-04 14:44:09 +0200

    Added year validator

--- src/components/DaysContainer/index.js
+++ src/components/DaysContainer/index.js
@@ -5,7 +5,8 @@ class DaysContainer extends React.Component {
   render() {
     const { events, selectedDate } = this.props;
     const filteredEvents = events.filter(event => (
-      selectedDate.getMonth() === event.dateStart.getMonth()
+      selectedDate.getMonth() === event.dateStart.getMonth() &&
+      selectedDate.getYear() === event.dateStart.getYear()
     ));
     const filteredDayTiles = filteredEvents.map(event => {
       const { id, title, dateStart, dateEnd, location, details } = event;
